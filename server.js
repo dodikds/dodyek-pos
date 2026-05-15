98278086643730e28236284b72cef42a901223ca
Dodik Dwi Sancoko <dodikds@gmail.com>
2026-05-15 21:25:48 +0700

    fix express wilcard route

--- server.js
+++ server.js
@@ -10,24 +10,23 @@ app.use(express.json())
 const itemsRoute = require('./routes/itemsRoute')
 const usersRoute = require('./routes/userRoute')
 const billsRoute = require('./routes/billsRoute')
+
 app.use('/api/items/', itemsRoute)
 app.use('/api/users/', usersRoute)
 app.use('/api/bills/', billsRoute)
-const path = require('path')
 
-if(process.env.NODE_ENV==='production')
-{
-    // app.use('/', express.static('client/build'))
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // app.get('/*', (req, res)=>{
-    //     res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-    // })
-    app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+// const path = require('path')
+
+// if(process.env.NODE_ENV==='production')
+// {
+//     app.use('/', express.static('client/build'))
+    
+//     app.get('/*', (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+//     })
+// }
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World! from home api'))
+app.get('/', (req, res) => res.send('POS Backend API Running'))
 app.listen(port, () => console.log(`Node JS Server Running port ${port}!`))
