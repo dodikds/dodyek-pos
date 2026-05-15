37c81acbd82d1ffe25945adf9315aa71970a7406
Dodik Dwi Sancoko <dodikds@gmail.com>
2026-05-15 19:46:48 +0700

    publish to Github

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ const billsRoute = require('./routes/billsRoute')
 app.use('/api/items/', itemsRoute)
 app.use('/api/users/', usersRoute)
 app.use('/api/bills/', billsRoute)
+const path = require('path')
 
-const port = 5000
+if(process.env.NODE_ENV==='production')
+{
+    app.use('/', express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+    })
+}
+
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World! from home api'))
 app.listen(port, () => console.log(`Node JS Server Running port ${port}!`))
